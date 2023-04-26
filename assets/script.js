002971cd9ce4ba934bdf4419d082d24487ae23bc
Emma B <emm2111@hotmail.fr>
2023-04-26 15:09:16 +0200

    boucle slider

--- assets/script.js
+++ assets/script.js
@@ -17,27 +17,43 @@ const slides = [
 	}
 ]
 
+// Récupération des éléments HTML à modifier
 let placeImage = document.querySelector(".banner-img")
 let placeTagLine = document.querySelector("#banner>p")
+
+// Variable "compteur" pour récupérer les indices tableaux
 let i = 0;
 
 
-
+// Fonction flèche gauche
 const arrowLeft = document.querySelector(".arrow_left")
 arrowLeft.addEventListener("click", function () {
+	// Décrémentation de l'indice
 	i--;
+	// Condition de retour à l'autre bout du tableau
+	if (i < 0){
+		i = slides.length - 1;}
+	// Modification du contenu html en fonction de l'indice du tableau slides
 	placeImage.src = "./assets/images/slideshow/" + slides[i].image
 	placeTagLine.innerHTML = slides[i].tagLine
 })
 
 console.log("flèche gauche ok")
 
+// Fonction flèche droite
 const arrowRight = document.querySelector(".arrow_right")
 arrowRight.addEventListener("click", function () {
+	// Incrémentation de l'indice
 	i++;
+	// Condition de retour à l'autre bout du tableau
+	if (i > slides.length - 1){
+		i = 0;}
+	// Modification du contenu html en fonction de l'indice du tableau slides
 	placeImage.src = "./assets/images/slideshow/" + slides[i].image
 	placeTagLine.innerHTML = slides[i].tagLine
 })
 
 console.log("flèche droite ok")
+
+
 
